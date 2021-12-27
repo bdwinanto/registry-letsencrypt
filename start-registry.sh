7bd7b9a95d939fa8d41beafa235d3e27c7407189
bdwinanto <90337629+bdwinanto@users.noreply.github.com>
2021-12-27 09:18:47 +0700

    Update start-registry.sh

--- start-registry.sh
+++ start-registry.sh
@@ -28,7 +28,7 @@ createuser() {
     read username
     echo -n "Password":
     read -s password
-    docker run --rm --entrypoint htpasswd registry:2 -Bbn ${username} ${password} > conf/auth/htpasswd
+    docker run --rm --entrypoint htpasswd registry:2.7.0 -Bbn ${username} ${password} > conf/auth/htpasswd
     log_lvl_info "Authentication configuration completed."
 }
 
